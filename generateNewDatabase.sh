40c566ae205f8c8312e1cbc64bd0db1c2ac9b49d
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-01-15 21:57:18 -0500

    Updated SQL script to match new DB setup

--- generateNewDatabase.sh
+++ generateNewDatabase.sh
@@ -2,6 +2,5 @@
 rm ./MemUp.db
 dotnet ef database update --context MemUpDbContext
 dotnet ef database update --context MemUpIdentityDbContext
-# Seed script needs to be updated 
-# sqlite3 MemUp.db < SeedDatabase.sql
+sqlite3 MemUp.db < SeedDatabase.sql
 echo "New database generated"

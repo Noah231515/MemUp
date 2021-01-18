10b5ae1eeb5bc513d7f79b968d287110608632df
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-01-18 15:14:34 -0500

    Create dashboard authentication resolver and add it to the routing module

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,35 +4,32 @@ import { CounterComponent } from "./counter/counter.component";
 import { HomeComponent } from "./home/home.component";
 import { CourseService } from "./services/course.service";
 import { UserDashboardComponent } from "./user-dashboard/user-dashboard.component";
-import { SubscribedCoursesResolver } from "./user-dashboard/user-dashboard.resolver";
+import { SubscribedCoursesResolver } from "./user-dashboard/subscribed-courses.resolver";
+import { DashboardAuthenticationResolver } from "./user-dashboard/dashboard-authentication.resolver";
 
 const routes: Routes = [
-    {
-      path: '',
-      redirectTo: 'home',
-      pathMatch: 'full',
-    },
-    {
-      path: 'home',
-      component: HomeComponent,
-      pathMatch: 'full',
-    },
-    {
-      path: 'dashboard',
-      component: UserDashboardComponent,
-      pathMatch: 'full',
-      resolve: {
-        subscribedCourses: SubscribedCoursesResolver
-      }
-    },
-    {
-      path: 'counter',
-      component: CounterComponent
-    },
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: UserDashboardComponent,
+    pathMatch: 'full',
+    resolve: {
+      subscribedCourses: SubscribedCoursesResolver,
+      isAuthenticated: DashboardAuthenticationResolver
+    }
+  },
+  {
+    path: 'counter',
+    component: CounterComponent
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }

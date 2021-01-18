48a3e9736ece7533aa08f893f556f7eae366fecc
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-01-17 22:32:52 -0500

    Change "Home" link on nav menu based on auth status

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,12 +1,17 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { AuthorizeService } from 'src/api-authorization/authorize.service';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
   styleUrls: ['./nav-menu.component.css']
 })
-export class NavMenuComponent {
+export class NavMenuComponent implements OnInit {
   isExpanded = false;
+  homeLink: string = "/";
+
+  constructor(private authorize: AuthorizeService){}
 
   collapse() {
     this.isExpanded = false;
@@ -14,5 +19,14 @@ export class NavMenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  ngOnInit(){
+    this.authorize.isAuthenticated()
+      .subscribe(isAuthenticated => {
+        if(isAuthenticated){
+          this.homeLink = "/dashboard";
+        }
+      })
   }
 }

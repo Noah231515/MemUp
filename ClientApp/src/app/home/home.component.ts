135ddf2ae5c7e7182e6bee6849ecee2622cb5aca
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-01-18 21:20:55 -0500

    Removed unused imports and ununsed ngOnInit method

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthorizeService } from 'src/api-authorization/authorize.service';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +7,5 @@ import { AuthorizeService } from 'src/api-authorization/authorize.service';
 export class HomeComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
+  
 }

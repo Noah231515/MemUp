df16f91aaa6e0391512332c033a7144d4d075763
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-01-19 19:02:06 -0500

    Create course-summary-card component

---
+++ ClientApp/src/app/user-dashboard/course-summary-card/course-summary-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-summary-card',
+  templateUrl: './course-summary-card.component.html',
+  styleUrls: ['./course-summary-card.component.css']
+})
+export class CourseSummaryCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

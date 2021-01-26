ec3a934af28240f79d25c53029b78f8634197f3d
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-01-26 14:20:39 -0500

    Add functions to subscribe to  and unsubscribe from courses

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace MemUp.Controllers
 {
@@ -27,7 +29,48 @@ namespace MemUp.Controllers
         [HttpGet]
         public async Task<IActionResult> GetSubscribedCoursesForUsers()
         {
-            return Ok(courses.Include(x => x.Words).First());
+            var user = await userManager.GetUserAsync(this.User);
+            List<Course> subscribedCourses = new List<Course>();
+            if (user != null)
+            {
+                var userCourses = memUpDbContext.UserCourse.Where(uc => uc.UserId == new System.Guid(user.Id));
+
+
+                foreach (UserCourse userCourse in userCourses)
+                {
+                    Course course = memUpDbContext.Courses.Find(userCourse.CourseId);
+                    subscribedCourses.Add(course);
+                }
+
+            }
+            return Ok(subscribedCourses);
+        }
+
+        [HttpPost]
+        [Route("/courses/subscribetocourse/{id}")]
+        public async Task<IActionResult> SubscribeToCourse(System.Guid id)
+        {
+            var user = await userManager.GetUserAsync(this.User);
+            Course course = memUpDbContext.Courses.Find(id);
+            UserCourse userCourse = new UserCourse()
+            {
+                Id = new System.Guid(),
+                UserId = new System.Guid(user.Id),
+                CourseId = course.Id
+            };
+            memUpDbContext.UserCourse.Add(userCourse);
+            return Ok(userCourse);
+        }
+
+        [HttpDelete]
+        [Route("/courses/unsubscribefromcourse/{id}")]
+        public async Task<IActionResult> UnsubscribeFromCourse(System.Guid id)
+        {
+            var user = await userManager.GetUserAsync(this.User);
+            UserCourse userCourse = memUpDbContext.UserCourse.Single(uc => uc.UserId == new Guid(user.Id) && uc.CourseId == id);
+            memUpDbContext.UserCourse.Remove(userCourse);
+            memUpDbContext.SaveChanges();
+            return Ok(userCourse);
         }
     }
 }

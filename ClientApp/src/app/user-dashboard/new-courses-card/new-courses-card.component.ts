21facd335d99b906ba739bc28bd1d0711cfe8b89
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-01-26 18:12:50 -0500

    create new courses card component

---
+++ ClientApp/src/app/user-dashboard/new-courses-card/new-courses-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-courses-card',
+  templateUrl: './new-courses-card.component.html',
+  styleUrls: ['./new-courses-card.component.css']
+})
+export class NewCoursesCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

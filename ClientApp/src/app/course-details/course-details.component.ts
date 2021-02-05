6f9762896ddac34decf8d1be9cb330b33d4c05a7
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-02-05 14:19:10 -0500

    Add sentence data to table and fix load times through progressive data loading

--- ClientApp/src/app/course-details/course-details.component.ts
+++ ClientApp/src/app/course-details/course-details.component.ts
@@ -1,9 +1,10 @@
 import { AfterViewInit, ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../models/course.model';
+import { Sentence } from '../models/sentence.model';
 import { Word } from '../models/word.model';
 
 @Component({
@@ -16,12 +17,13 @@ export class CourseDetailsComponent implements OnInit, AfterViewInit {
   public displayedColumns: string[] = ['Japanese Word', 'English', 'Sentence'];
   public dataSource: MatTableDataSource<Word>;
   @ViewChild(MatPaginator) public paginator: MatPaginator;
+  public DATA_CHUNK_SIZE = 500;
 
   public constructor(private route: ActivatedRoute) { }
 
   public ngOnInit(): void {
     this.course = this.route.snapshot.data['course'];
-    this.dataSource = new MatTableDataSource<Word>(this.course.words);
+    this.dataSource = new MatTableDataSource<Word>(this.course.words.slice(0, this.DATA_CHUNK_SIZE));
     console.log(this.course);
   }
 
@@ -29,4 +31,14 @@ export class CourseDetailsComponent implements OnInit, AfterViewInit {
     this.dataSource.paginator = this.paginator;
   }
 
+  public checkForAdditionalTableData(pageEvent: PageEvent) {
+    if (!this.paginator.hasNextPage()) {
+      if (pageEvent.length + this.DATA_CHUNK_SIZE < this.course.words.length) {
+        this.dataSource = new MatTableDataSource<Word>(this.course.words.slice(0, pageEvent.length + this.DATA_CHUNK_SIZE));
+      } else {
+        this.dataSource = new MatTableDataSource<Word>(this.course.words);
+      }
+      this.dataSource.paginator = this.paginator;
+    }
+  }
 }

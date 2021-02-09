9f430874fe89c2628971b783b5d2e4a726748a87
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-02-09 13:13:37 -0500

    Create course-details-card component

---
+++ ClientApp/src/app/course-details/course-details-card/course-details-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-details-card',
+  templateUrl: './course-details-card.component.html',
+  styleUrls: ['./course-details-card.component.css']
+})
+export class CourseDetailsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

ecbb73c6a6159d4973efb2a9e3aa4e4c053d08fb
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-02-15 14:47:14 -0500

    Add DescriptionFull property to Course model

---
+++ Models/Migrations/20210215194349_AddCourseDescriptionFull.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MemUp.Migrations
+{
+    public partial class AddCourseDescriptionFull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionFull",
+                table: "Courses",
+                type: "TEXT",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescriptionFull",
+                table: "Courses");
+        }
+    }
+}

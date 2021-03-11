6d81cc4c4f3359945069559b964356bde738861e
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-11 17:03:03 -0500

    Make use of new course editing components

--- ClientApp/src/app/course-manager/course-manager.component.ts
+++ ClientApp/src/app/course-manager/course-manager.component.ts
@@ -14,71 +14,21 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class CourseManagerComponent implements OnInit {
   @Input() public course: Course;
-  @Output() public updateCourse = new EventEmitter<Course>();
-  public manageCourseForm: FormGroup;
+  @Output() public courseUpdated = new EventEmitter<Course>();
   public contentToEdit = 'details';
-  public wordToEdit: Word;
-  public updatedWords: Word[] = [];
 
 
-  public constructor(
-    private formBuilder: FormBuilder,
-    private courseService: CourseService,
-    private wordSerrvice: WordService) { }
+  public constructor() { }
 
   public ngOnInit(): void {
-    this.initializeForm();
-  }
-
-  public initializeForm(): void {
-    this.manageCourseForm = this.formBuilder.group({
-      courseName: this.course.name,
-      courseDescShort: this.course.description,
-      courseDescFull: this.course.descriptionFull,
-    });
   }
 
   public changeEditSection(value: string): void {
       this.contentToEdit = value;
   }
 
-  public clearWordToEdit(): void {
-    this.wordToEdit = undefined;
-  }
-
-  public submitForm(): void {
-    const formValues = this.manageCourseForm.value;
-
-    // Submit the updated words to the database if there are any
-    if (this.updatedWords.length > 0) {
-      this.wordSerrvice.updateWords(this.updatedWords).subscribe();
-    }
-
-    const updatedCourse: Course = {
-      id: this.course.id,
-      name: formValues.courseName,
-      description: formValues.courseDescShort,
-      descriptionFull: formValues.courseDescFull,
-      words: this.course.words
-    };
-
-    this.courseService.updateCourse(updatedCourse).subscribe((_updatedCourse) => {
-      this.course = _updatedCourse;
-      this.updateCourse.emit(_updatedCourse);
-    });
-  }
-
-  public clearForm(): void {
-    this.manageCourseForm.reset();
-    this.wordToEdit = undefined;
-  }
-
-  public setWordToEdit(selectedWord: Word): void {
-    this.wordToEdit = selectedWord;
-  }
-
-  public addUpdatedWord(word: Word): void {
-    this.updatedWords.push(word);
+  public updateCourse(updatedCourse: Course) {
+    this.courseUpdated.emit(updatedCourse);
   }
 
 }

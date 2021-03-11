16f6b3060d9530f3f361929201c09d48928dc989
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-11 17:30:41 -0500

    Remove unused imports

--- ClientApp/src/app/course-manager/course-manager.component.ts
+++ ClientApp/src/app/course-manager/course-manager.component.ts
@@ -1,11 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Component, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { Course } from '../models/course.model';
-import { Word } from '../models/word.model';
-import { CourseService } from '../services/course.service';
-import { WordService } from '../services/word.service';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-course-manager',

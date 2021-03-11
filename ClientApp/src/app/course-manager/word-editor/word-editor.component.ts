9e08815fcd32194f325081b3734d50a784f177af
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-10 20:19:16 -0500

    Create word editor component

---
+++ ClientApp/src/app/course-manager/word-editor/word-editor.component.ts
@@ -0,0 +1,46 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Word } from 'src/app/models/word.model';
+
+@Component({
+  selector: 'app-word-editor',
+  templateUrl: './word-editor.component.html',
+  styleUrls: ['./word-editor.component.css']
+})
+export class WordEditorComponent implements OnInit {
+  public wordEditorForm: FormGroup;
+  @Input() public word: Word;
+  @Output() public wordModified = new EventEmitter<Word>();
+
+
+  public constructor(private formBuilder: FormBuilder) { }
+
+  public ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  public initializeForm(): void {
+    this.wordEditorForm = this.formBuilder.group({
+      englishVocab: this.word.englishVocab,
+      japaneseVocab: this.word.japaneseVocab,
+      kanaVocab: this.word.kanaVocab,
+      partOfSpeech: this.word.partOfSpeech,
+    });
+  }
+
+  public submitForm(): void {
+    const formValues = this.wordEditorForm.value;
+
+    const modifiedWord: Word = {
+      id: this.word.id,
+      englishVocab: this.word.englishVocab === formValues.englishVocab ? this.word.englishVocab : formValues.englishVocab,
+      japaneseVocab: this.word.japaneseVocab === formValues.japaneseVocab ? this.word.japaneseVocab : formValues.japaneseVocab,
+      kanaVocab: this.word.kanaVocab === formValues.kanaVocab ? this.word.kanaVocab : formValues.kanaVocab,
+      partOfSpeech: this.word.partOfSpeech === formValues.partOfSpeech ? this.word.partOfSpeech : formValues.partOfSpeech,
+      sentences: this.word.sentences
+    };
+
+    this.wordModified.emit(modifiedWord);
+  }
+
+}

b30690aa88d0da8f985befde84abb97ea6c00b95
Andrew Bumpus <andrewpbumpus@gmail.com>
2021-03-19 19:01:47 -0400

    Add method for creating blank sentences

--- ClientApp/src/app/services/word.service.ts
+++ ClientApp/src/app/services/word.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Sentence } from '../models/sentence.model';
 import { Word } from '../models/word.model';
 
 @Injectable({
@@ -55,5 +56,24 @@ export class WordService {
 
   public getAllWords(): Observable<Word[]> {
     return this.http.get<Word[]>('/words/getallwords');
+  }
+
+  /**
+   * Creates a blank sentence using the sentence type and word provided in params
+   * @param {string} sentenceType
+   * @param {Word} word
+   * @return {*}  {Sentence}
+   * @memberof WordService
+   */
+  public createBlankSentence(sentenceType: string, word: Word): Sentence {
+    const blankSentence: Sentence = {
+      sentenceText: '',
+      word: word,
+      sentenceType: {
+        type: sentenceType,
+      },
+    };
+
+    return blankSentence;
   }
 }

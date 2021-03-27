a6b90097b2767507b1de27e2e737c5ed98e8cd00
Noah <Noah231515@gmail.com>
2021-03-27 15:09:30 -0400

    add config, remove users from memUpDbContext

--- Models/MemUpDbContext.cs
+++ Models/MemUpDbContext.cs
@@ -88,7 +88,6 @@ namespace MemUp.Models
             }
         }
 
-        public DbSet<ApplicationUser> ApplicationUser { get; set; }
         public DbSet<Course> Courses { get; set;}
         public DbSet<Word> Word { get; set; }
         public DbSet<Sentence> Sentence { get; set; }

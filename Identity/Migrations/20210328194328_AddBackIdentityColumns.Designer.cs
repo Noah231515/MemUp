8aefae39e0f16bbe69c675decd3d35e123e176e4
Noah <Noah231515@gmail.com>
2021-03-28 16:22:23 -0400

    add back columns idnetity needed

--- Identity/Migrations/20210328194328_AddBackIdentityColumns.Designer.cs
+++ Identity/Migrations/20210328194328_AddBackIdentityColumns.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using MemUp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MemUp.Data.Migrations
 {
     [DbContext(typeof(MemUpIdentityDbContext))]
-    partial class MemUpIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210328194328_AddBackIdentityColumns")]
+    partial class AddBackIdentityColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

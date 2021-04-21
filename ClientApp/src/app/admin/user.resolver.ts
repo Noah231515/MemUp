f5d599bf6fd5c31ced3f708b1e7d92987d8575ba
Noah <Noah231515@gmail.com>
2021-04-20 20:16:35 -0400

    requested changes

--- ClientApp/src/app/admin/user.resolver.ts
+++ ClientApp/src/app/admin/user.resolver.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Course } from '../models/course.model';
-import { UserCourse } from '../models/usercourse.model';
 import { Resolve } from '@angular/router';
+import { UserDto } from '../models/user-dto';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsersResolver implements Resolve<any> {
+export class UsersResolver implements Resolve<UserDto[]> {
 
   public constructor(private http: HttpClient) { }
 
   /**
    * Returns all users
    */
-  public resolve(): any {
+  public resolve(): Observable<UserDto[]> {
     return this.getAllUsers();
   }
 
   /**
    * Returns all users
    */
-  public getAllUsers(): Observable<any[]> {
+  public getAllUsers(): Observable<UserDto[]> {
     return this.http.get<any[]>('/users/getallusers');
   }
 }

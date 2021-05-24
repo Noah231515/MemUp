3e811a274e72e6617891e5ba897b491b8213e200
AndrewB21 <andrewpbumpus@gmail.com>
2021-05-24 19:17:36 -0400

    Create word table component

---
+++ ClientApp/src/app/courses/word-table/word-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-word-table',
+  templateUrl: './word-table.component.html',
+  styleUrls: ['./word-table.component.css']
+})
+export class WordTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

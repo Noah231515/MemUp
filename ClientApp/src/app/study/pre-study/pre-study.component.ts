9208ab203bd023c60ce025767cde6e7dfc982825
AndrewB21 <andrewpbumpus@gmail.com>
2021-05-25 16:05:31 -0400

    Stub in basic pre study component layout

--- ClientApp/src/app/study/pre-study/pre-study.component.ts
+++ ClientApp/src/app/study/pre-study/pre-study.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course.model';
+import { Word } from 'src/app/models/word.model';
 
 @Component({
   selector: 'app-pre-study',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pre-study.component.css']
 })
 export class PreStudyComponent implements OnInit {
+  @Input() course: Course;
+  @Input() allCourses: Course[];
+  public sessionSize: number;
+  public selectedWords: Word[];
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    this.selectedWords = this.course.words.slice(0, 25);
+    this.allCourses.splice(this.allCourses.findIndex(element => element.id === this.course.id),1)
   }
 
 }

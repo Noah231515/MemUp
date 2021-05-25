b9f054b976e2b2e29a41e499427d55f8b102143e
AndrewB21 <andrewpbumpus@gmail.com>
2021-05-25 17:16:01 -0400

    Implement study options functionality

--- ClientApp/src/app/study/pre-study/pre-study.component.ts
+++ ClientApp/src/app/study/pre-study/pre-study.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Course } from 'src/app/models/course.model';
 import { Word } from 'src/app/models/word.model';
+import { SnackBarService } from 'src/app/services/snack-bar.service';
 
 @Component({
   selector: 'app-pre-study',
@@ -10,15 +11,50 @@ import { Word } from 'src/app/models/word.model';
 export class PreStudyComponent implements OnInit {
   @Input() course: Course;
   @Input() allCourses: Course[];
+  @Output() public updateSize = new EventEmitter<number>();
+  @Output() public updateType = new EventEmitter<string>();
+  @Output() public updateCourse= new EventEmitter<string>();
   public sessionSize: number;
   public selectedWords: Word[];
   
 
-  constructor() { }
+  constructor(private snackBarService: SnackBarService) { }
 
   ngOnInit(): void {
     this.selectedWords = this.course.words.slice(0, 25);
-    this.allCourses.splice(this.allCourses.findIndex(element => element.id === this.course.id),1)
+  }
+
+  updateOptions(updatedOption: string, updatedValue: any) {
+    const invalidValueErorr = new Error("Invalid value provided.");
+
+    try{
+      switch (updatedOption) {
+        case 'size':
+          console.log(typeof(updatedValue))
+            if (parseInt(updatedValue) !== NaN) {
+              this.updateSize.emit(updatedValue);
+              break;
+            } else {
+              throw invalidValueErorr;
+            }
+        case 'type':
+          if(typeof(updatedValue) === "string") {
+            this.updateType.emit(updatedValue);
+            break;
+          } else {
+            throw invalidValueErorr;
+          }
+        case 'course':
+          if(typeof(updatedValue) === 'string') {
+            this.updateCourse.emit(updatedValue);
+            break;
+          } else {
+            throw invalidValueErorr;
+          }
+      }
+    } catch (e) {
+      this.snackBarService.openSnackBar(e);
+    }
   }
 
 }
--- ClientApp/src/app/study/study.component.ts
+++ ClientApp/src/app/study/study.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../models/course.model';
+import { SnackBarService } from '../services/snack-bar.service';
 
 @Component({
   selector: 'app-study',
@@ -6,10 +9,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./study.component.css']
 })
 export class StudyComponent implements OnInit {
-
-  constructor() { }
+  public courseList: Course[];
+  public selectedCourse: Course;
+  public isPreStudy: boolean;
+  public isStudyGame: boolean;
+  public selectedOptions: StudyOptions = {
+    sessionSize: 10,
+    sessionType: 'multipleChoice'
+  };
+  
+  constructor(
+    private route: ActivatedRoute,
+    private snackBarService: SnackBarService,
+  ) { }
 
   ngOnInit(): void {
+    this.isPreStudy = true;
+    this.courseList = this.route.snapshot.data['courseList'];
+    this.selectedCourse = this.route.snapshot.data['selectedCourse'];
   }
 
+  public changeSessionMode() {
+    this.isPreStudy = !this.isPreStudy;
+    this.isStudyGame = !this.isStudyGame;
+  }
+
+  public updateCourse(newCourseName: string) {
+    this.selectedCourse = this.courseList.find(element => element.name === newCourseName);
+  }
+
+  public updateSessionSize(newSize: number) {
+    this.selectedOptions.sessionSize = newSize;
+  }
+
+  public updateSessionType(newType: string) {
+    const validSessionTypes = ['multipleChoice', 'typingChallenge', 'mixed'];
+    try {
+      if (validSessionTypes.find(element => element === newType)) {
+        this.selectedOptions.sessionType = newType;
+      } else {
+        throw new Error('An invalid session type was provided.')
+      }
+    } catch (error) { 
+      this.snackBarService.openSnackBar(error)
+    }
+  }
+
+}
+
+interface StudyOptions {
+  sessionSize: number,
+  sessionType: string,
 }

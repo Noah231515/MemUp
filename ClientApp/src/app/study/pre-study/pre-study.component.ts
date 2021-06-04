bb6c00b5b4f65b6d80fff673392e3d206b18aea3
AndrewB21 <andrewpbumpus@gmail.com>
2021-06-03 20:31:19 -0400

    Break updateOptions method into separate update methods for each option

--- ClientApp/src/app/study/pre-study/pre-study.component.ts
+++ ClientApp/src/app/study/pre-study/pre-study.component.ts
@@ -12,48 +12,51 @@ import { SnackBarService } from 'src/app/services/snack-bar.service';
 export class PreStudyComponent implements OnInit {
   @Input() public studyOptions: StudyOptions;
   @Input() public allCourses: Course[];
-  @Output() public updateSize = new EventEmitter<number>();
-  @Output() public updateType = new EventEmitter<string>();
-  @Output() public updateCourse = new EventEmitter<string>();
+  @Output() public sizeUpdated = new EventEmitter<number>();
+  @Output() public typeUpdated = new EventEmitter<string>();
+  @Output() public courseUpdated = new EventEmitter<string>();
   public selectedWords: Word[];
+  public SESSION_TYPES = {
+    multipleChoice: 'multipleChoice',
+    typingChallenge: 'typingChallenge',
+    mixed: 'mixed',
+  }
 
-  constructor(private snackBarService: SnackBarService) { }
+  constructor() { }
 
   public ngOnInit(): void {
     this.selectedWords = this.studyOptions.course.words.slice(0, this.studyOptions.sessionSize);
   }
 
-  public updateOptions(updatedOption: string, updatedValue: any) {
-    const invalidValueErorr = new Error('Invalid value provided.');
+  /**
+   * Updates the session size from based on the session size input value and emits the new value to the Study component
+   *
+   * @param {number} updatedSize
+   * @memberof PreStudyComponent
+   */
+  public updateSize (updatedSize: number) {
+    this.sizeUpdated.emit(updatedSize);
+    this.selectedWords = this.studyOptions.course.words.slice(0, updatedSize);
+  }
 
-    try {
-      switch (updatedOption) {
-        case 'size':
-            if (parseInt(updatedValue, 10) !== NaN) {
-              this.updateSize.emit(updatedValue);
-              this.selectedWords = this.studyOptions.course.words.slice(0, updatedValue);
-              break;
-            } else {
-              throw invalidValueErorr;
-            }
-        case 'type':
-          if (typeof(updatedValue) === 'string') {
-            this.updateType.emit(updatedValue);
-            break;
-          } else {
-            throw invalidValueErorr;
-          }
-        case 'course':
-          if (typeof(updatedValue) === 'string') {
-            this.updateCourse.emit(updatedValue);
-            break;
-          } else {
-            throw invalidValueErorr;
-          }
-      }
-    } catch (e) {
-      this.snackBarService.openSnackBar(e);
-    }
+  /**
+   * Updates the selected course from based on the course input value and emits the new value to the Study component
+   *
+   * @param {string} updatedCourseName
+   * @memberof PreStudyComponent
+   */
+  public updateCourse (updatedCourseName: string) {
+    this.courseUpdated.emit(updatedCourseName);
+  }
+
+  /**
+   * Updates the session type from based on the session type input value and emits the new value to the Study component
+   *
+   * @param {string} updatedType
+   * @memberof PreStudyComponent
+   */
+  public updateType (updatedType: string) {
+    this.typeUpdated.emit(updatedType);
   }
 
 }

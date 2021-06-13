2923a866a5f309a07b0a8c70fc036a77abb65d9b
Noah <Noah231515@gmail.com>
2021-06-13 09:48:41 -0400

    add expande rcolumns from docs

--- ClientApp/src/app/admin/user-list/user-list.component.ts
+++ ClientApp/src/app/admin/user-list/user-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserDto } from 'src/app/models/user-dto';
@@ -6,9 +7,17 @@ import { UserDto } from 'src/app/models/user-dto';
   encapsulation: ViewEncapsulation.None,
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', visibility: 'hidden' })),
+      state('expanded', style({ height: '*', visibility: 'visible' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class UserListComponent implements OnInit {
+  public expandedElement: any;
 
   public users: UserDto[];
   public displayedColumns: string[] = ['userName', 'roles'];
@@ -21,5 +30,7 @@ export class UserListComponent implements OnInit {
       this.users = data.users;
     });
   }
+
+  isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow');
 
 }
